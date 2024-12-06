b1ef1c5d083143e22723d025ab6357e3cc28c7b9
Quentin Lem <quentinlemcode@gmail.com>
2024-12-06 18:22:23 +0100

    add cache

--- lib.ts
+++ lib.ts
@@ -1,11 +1,21 @@
 
 import 'dotenv/config';
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from 'fs';
 
 export async function getInput(day: number): Promise<string> {
+    const cacheFile = 'cache/' + day;
+    if(existsSync(cacheFile)) {
+        return readFileSync(cacheFile).toString();
+    }
     const response = await fetch(`https://adventofcode.com/2024/day/${day}/input`, {
         headers: {
             Cookie: process.env.AOC_COOKIE || ''
         }
     });
-    return response.text();
+    const result = await response.text();
+    if (!existsSync('cache')) {
+        mkdirSync('cache');
+    }
+    writeFileSync(cacheFile, result)
+    return result;
 }
