bb2f37567967e3034f8c7c746c8986ed8662eb60
Quentin Lem <quentinlemcode@gmail.com>
2024-12-06 17:27:58 +0100

    day 5 part 2

--- day5.ts
+++ day5.ts
@@ -1,3 +1,4 @@
+import { writeFileSync } from 'fs';
 import { getInput } from './lib';
 
 const example = `47|53
@@ -27,28 +28,65 @@ const example = `47|53
 75,29,13
 75,97,47,61,53
 61,13,29
-97,13,75,29,47`
+97,13,75,29,47`;
 
 const input = await getInput(5);
 
 const parse = (str: string) => {
-    const [orders, updates] = str.split('\n\n');
-    return { orders: orders.split('\n').map(a => a.split('|').map(Number)), updates: updates.split('\n').map(a => a.split(',').map(Number)) };
-}
+  const [orders, updates] = str.split('\n\n');
+  return {
+    orders: orders.split('\n').map((a) => a.split('|').map(Number)),
+    updates: updates.split('\n').map((a) => a.split(',').map(Number)),
+  };
+};
 
-const {orders, updates} = parse(input);
+const { orders, updates } = parse(input);
 
-const validUpdates = updates.filter((update => {
-    for (let i = 1; i < update.length; i++) {
-        const numToCheck = update[i];
-        for (const [first, second] of orders) {
-            if (numToCheck !== first) continue;
-            if (update.slice(0,i).includes(second)) return false;
-        }
+const validUpdates = updates.filter((update) => {
+  for (let i = 1; i < update.length; i++) {
+    const numToCheck = update[i];
+    for (const [first, second] of orders) {
+      if (numToCheck !== first) continue;
+      if (update.slice(0, i).includes(second)) return false;
     }
-    return true;
-}))
+  }
+  return true;
+});
 
-const result = validUpdates.map(v => v[Math.floor(v.length / 2)]);
+const result = validUpdates.map((v) => v[Math.floor(v.length / 2)]);
 
-console.log(result.reduce((a,b) => a + b, 0));
+console.log(result.reduce((a, b) => a + b, 0));
+
+// part 2
+
+const swap = (arr: number[], i: number, j: number) => {
+    const newArr = [...arr];
+    newArr[i] = arr[j];
+    newArr[j] = arr[i];
+    return newArr;
+    };
+
+const validateUpdate = (update: number[], updated = false) => {
+  for (let i = 1; i < update.length; i++) {
+    const numToCheck = update[i];
+    for (const [first, second] of orders) {
+      if (numToCheck !== first) continue;
+      const idx = update.slice(0, i).indexOf(second);
+      if (idx !== -1) {
+          return validateUpdate(
+            swap(update, i, idx),
+          true
+        );
+      }
+    }
+  }
+  return { updated, update };
+};
+
+console.log(
+  updates
+    .map((u) => validateUpdate(u))
+    .filter((u) => u.updated)
+    .map((u) => u.update[Math.floor(u.update.length / 2)])
+    .reduce((a, b) => a + b, 0)
+);
